2f8c89b06687db5c5e3f7d0d37d1d286898d3b6a
GersonESS <gebhsantos@gmail.com>
2025-01-23 22:34:05 -0300

    Update app.js

--- 05 - Fra/app.js
+++ 05 - Fra/app.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+// Import the express library
+const express = require('express');
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.write('Hello World!');
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`Rodando app listening at http://localhost:${port}`);
+  });
+
+// Define the port
+const port = 8080;
+
