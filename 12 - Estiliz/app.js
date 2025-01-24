0347ed2387d9b6ebfce2e5f9c3a6fdbd1e1f8fbe
GersonESS <gebhsantos@gmail.com>
2025-01-24 00:36:49 -0300

    s

---
+++ 12 - Estiliz/app.js
@@ -0,0 +1,38 @@
+// Import the express library
+const express = require('express');
+
+// Importa o módulo express-handlebars
+const { engine } = require('express-handlebars');
+
+// importa o módulo mysql
+const mysql = require('mysql2');
+// App
+const app = express();
+
+// Adiciona o bootstrap
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+// configuração do handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+// configuração do banco de dados
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'projeto'
+}); 
+// Conectar
+conexao.connect(function(err){
+    if(err) throw err;
+    console.log('Conectado com sucesso!');
+});
+// Rota Principal
+app.get('/', (req, res) => {
+    res.render('formulario');
+});
+
+app.listen(8080, () => {
+    console.log('Rodando app listening at http://localhost:8080');
+  });
