6ac1c505433388538cdc1545ac49993abb655d7b
GersonESS <gebhsantos@gmail.com>
2025-01-23 23:55:33 -0300

    d

--- 09 - Utili/app.js
+++ 09 - Utili/app.js
@@ -1,9 +1,17 @@
 // Import the express library
 const express = require('express');
+
+// Importa o módulo express-handlebars
+const { engine } = require('express-handlebars');
+
 // importa o módulo mysql
 const mysql = require('mysql2');
 // App
 const app = express();
+// configuração do handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 // configuração do banco de dados
 
 const conexao = mysql.createConnection({
@@ -17,10 +25,9 @@ conexao.connect(function(err){
     if(err) throw err;
     console.log('Conectado com sucesso!');
 });
-
+// Rota Principal
 app.get('/', (req, res) => {
-    res.write('Hello World!');
-    res.end();
+    res.render('formulario');
 });
 
 app.listen(8080, () => {
